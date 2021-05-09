eb1fefdc1f85bf2a55a96ce96c725f4d734f6174
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-05-09 21:38:36 +0700

    Initial commit

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,59 @@
+import MDXComponents from '@/components/MDXComponents'
+import PageTitle from '@/components/PageTitle'
+import PostLayout from '@/layouts/PostLayout'
+import generateRss from '@/lib/generate-rss'
+import { formatSlug, getAllFilesFrontMatter, getFileBySlug, getFiles } from '@/lib/mdx'
+import fs from 'fs'
+import hydrate from 'next-mdx-remote/hydrate'
+
+export async function getStaticPaths() {
+  const posts = getFiles('blog')
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: formatSlug(p).split('/'),
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const allPosts = await getAllFilesFrontMatter('blog')
+  const postIndex = allPosts.findIndex((post) => formatSlug(post.slug) === params.slug.join('/'))
+  const prev = allPosts[postIndex + 1] || null
+  const next = allPosts[postIndex - 1] || null
+  const post = await getFileBySlug('blog', params.slug)
+
+  // rss
+  const rss = generateRss(allPosts)
+  fs.writeFileSync('./public/index.xml', rss)
+
+  return { props: { post, prev, next } }
+}
+
+export default function Blog({ post, prev, next }) {
+  const { mdxSource, frontMatter } = post
+  const content = hydrate(mdxSource, {
+    components: MDXComponents,
+  })
+
+  return (
+    <>
+      {frontMatter.draft !== true ? (
+        <PostLayout frontMatter={frontMatter} prev={prev} next={next}>
+          {content}
+        </PostLayout>
+      ) : (
+        <div className="mt-24 text-center">
+          <PageTitle>
+            Under Construction{' '}
+            <span role="img" aria-label="roadwork sign">
+              🚧
+            </span>
+          </PageTitle>
+        </div>
+      )}
+    </>
+  )
+}
