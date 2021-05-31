cffa13be530e9af916e2810d4740ac1e46fdbd75
Thang Vu <thvu@hey.com>
2021-05-31 22:04:55 +0700

    feat: spotify integration

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+import NowPlaying from '@/components/NowPlaying'
 
 export default function Footer() {
   return (
@@ -19,7 +20,8 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        <NowPlaying />
+        <div className="mb-4 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
             Tailwind Nextjs Theme
           </Link>
