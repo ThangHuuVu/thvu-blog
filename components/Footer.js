eb3cfee8894eb34d31c0eafe2c7657171bec59ae
Thang Vu <thvu@hey.com>
2021-06-14 09:09:11 +0700

    chore: remove footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,11 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
-import SocialIcon from '@/components/social-icons'
 import NowPlaying from '@/components/NowPlaying'
 
 export default function Footer() {
   return (
     <footer>
       <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-        </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
