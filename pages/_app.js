015b13689cb15f9721c725feb2cf78dc80f5db63
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-04 17:33:48 +0700

    feat: add robots.txt

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
           <Head>
             <meta
               name="viewport"
-              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
             />
           </Head>
           <DefaultSeo {...SEO} />
