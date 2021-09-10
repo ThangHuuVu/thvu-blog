59901465c7e6fc5a7552df03487fd76507f3c1db
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-10 18:06:46 +0700

    Opt-out of Google FLoC

--- next.config.js
+++ next.config.js
@@ -51,6 +51,12 @@ const securityHeaders = [
     key: 'Strict-Transport-Security',
     value: 'max-age=31536000; includeSubDomains; preload',
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // Opt-out of Google FLoC: https://amifloced.org/
+  {
+    key: 'Permissions-Policy',
+    value: 'interest-cohort=()',
+  },
 ]
 
 const nextConfig = {
