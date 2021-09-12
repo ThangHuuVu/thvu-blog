94bd167f53f5523e7022777183f4b5e741932be8
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-12 10:19:36 +0700

    Adjust headings

--- pages/projects.js
+++ pages/projects.js
@@ -11,27 +11,25 @@ export default function Projects({ allProjects }) {
         description={siteMetadata.description}
         url={`${siteMetadata.siteUrl}/projects`}
       />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Projects I built in my free time for my hobbies or personal needs.
-          </p>
-        </div>
-        <div className="container py-12">
-          <div className="flex flex-wrap -m-4">
-            {allProjects.map((d) => (
-              <Card
-                key={d.title}
-                title={d.title}
-                description={d.description}
-                imgSrc={d.imagesrc}
-                href={d.url}
-              />
-            ))}
-          </div>
+      <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Projects
+        </h1>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 xl:text-xl">
+          Projects I built in my free time for my hobbies or personal needs.
+        </p>
+      </div>
+      <div className="container py-12">
+        <div className="flex flex-wrap -m-4">
+          {allProjects.map((d) => (
+            <Card
+              key={d.title}
+              title={d.title}
+              description={d.description}
+              imgSrc={d.imagesrc}
+              href={d.url}
+            />
+          ))}
         </div>
       </div>
     </>
