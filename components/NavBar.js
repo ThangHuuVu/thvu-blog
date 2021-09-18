3f4a9de54661ebb8473b0c07489825a3396f09ba
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-18 10:34:08 +0700

    Add glassmorphic effect for side menu

--- components/NavBar.js
+++ components/NavBar.js
@@ -19,72 +19,74 @@ export default function NavBar() {
   }
 
   return (
-    <header className="h-28 w-full sticky z-10 top-0 bg-white dark:bg-black bg-opacity-30 dark:bg-opacity-30 backdrop-filter backdrop-blur-lg firefox:bg-opacity-100 dark:firefox:bg-opacity-100 border-b border-gray-200 dark:border-gray-800 flex items-center justify-between py-10">
-      <nav className="w-full max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0 flex items-center justify-end">
-        <div className=" flex items-center text-base leading-5">
-          <div className="hidden sm:block">
-            {headerNavLinks.map((link) => (
+    <>
+      <header className="h-28 w-full sticky z-10 top-0 bg-white dark:bg-black bg-opacity-30 dark:bg-opacity-30 backdrop-filter backdrop-saturate-150 backdrop-blur-lg firefox:bg-opacity-100 dark:firefox:bg-opacity-100 border-b border-gray-200 dark:border-gray-800 flex items-center justify-between py-10">
+        <nav className="w-full max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0 flex items-center justify-end">
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <div className="sm:hidden">
+              <button
+                type="button"
+                className="w-8 h-8 ml-1 mr-1 rounded"
+                aria-label="Toggle Menu"
+                onClick={onToggleNav}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="text-gray-900 dark:text-gray-100"
+                >
+                  {navShow ? (
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  ) : (
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    />
+                  )}
+                </svg>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
+      {/* Mobile side menu */}
+      <div
+        className={`md:hidden fixed w-full h-screen top-28 right-0 bg-white dark:bg-black z-10 transform ease-in-out duration-300 ${
+          navShow ? 'translate-x-0' : 'translate-x-full'
+        } backdrop-filter bg-opacity-30 dark:bg-opacity-30 backdrop-saturate-150 backdrop-blur-lg firefox:bg-opacity-100 dark:firefox:bg-opacity-100`}
+      >
+        <nav className="h-full mt-8">
+          {headerNavLinks.map((link) => (
+            <div key={link.title} className="px-12 py-4">
               <Link
-                key={link.title}
                 href={link.href}
-                className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
+                className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
+                onClick={onToggleNav}
               >
                 {link.title}
               </Link>
-            ))}
-          </div>
-          <div
-            className={`fixed w-full h-screen top-28 right-0 bg-white dark:bg-black z-10 transform ease-in-out duration-300 ${
-              navShow ? 'translate-x-0' : 'translate-x-full'
-            }`}
-          >
-            <div className="fixed h-full mt-8">
-              {headerNavLinks.map((link) => (
-                <div key={link.title} className="px-12 py-4">
-                  <Link
-                    href={link.href}
-                    className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
-                    onClick={onToggleNav}
-                  >
-                    {link.title}
-                  </Link>
-                </div>
-              ))}
             </div>
-          </div>
-
-          <ThemeSwitch />
-          <div className="sm:hidden">
-            <button
-              type="button"
-              className="w-8 h-8 ml-1 mr-1 rounded"
-              aria-label="Toggle Menu"
-              onClick={onToggleNav}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="text-gray-900 dark:text-gray-100"
-              >
-                {navShow ? (
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                ) : (
-                  <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                  />
-                )}
-              </svg>
-            </button>
-          </div>
-        </div>
-      </nav>
-    </header>
+          ))}
+        </nav>
+      </div>
+    </>
   )
 }
