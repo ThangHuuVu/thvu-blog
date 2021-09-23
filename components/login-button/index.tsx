5f06f36aec2edea50cce64ede7ca7e01cf480b35
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-24 00:18:32 +0700

    Attempt to fix next-auth redirect #2

--- components/login-button/index.tsx
+++ components/login-button/index.tsx
@@ -5,6 +5,7 @@ import GoogleDark from "./google-dark.svg";
 import Github from "./github.svg";
 import GithubDark from "./github-dark.svg";
 import { useDarkTheme } from "@/lib/hooks/useDarkTheme";
+import { useRouter } from "next/router";
 
 interface StyleGuide {
   Logo: any;
@@ -40,6 +41,7 @@ const providerStyleGuides: { [key: string]: StyleGuide } = {
 
 export default function LoginButton({ provider }: { provider: ClientSafeProvider }) {
   const [isDark] = useDarkTheme();
+  const router = useRouter();
   const { Logo, LogoDark, bg, bgDark, text, textDark } = providerStyleGuides[provider.id];
   return (
     <div key={provider.name}>
@@ -47,7 +49,7 @@ export default function LoginButton({ provider }: { provider: ClientSafeProvider
         className={`w-56 h-12 flex items-center gap-3 text-sm font-medium leading-5 transition-colors duration-150 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue
          ${isDark && bgDark ? bgDark : bg}
          ${isDark && textDark ? textDark : text} `}
-        onClick={() => signIn(provider.id)}
+        onClick={() => signIn(provider.id, { callbackUrl: router.query.callbackUrl as string })}
       >
         {isDark && LogoDark ? <LogoDark className="h-full p-2" /> : <Logo className="h-full p-2" />}
         Sign in with {provider.name}
