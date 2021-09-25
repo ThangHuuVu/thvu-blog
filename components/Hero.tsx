eb2655320af7d0c154b5c8a46822f6c2d7ef01b7
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-25 21:10:50 +0700

    refactor

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,51 +1,23 @@
 export default function Hero() {
+  const data = ["Front-end", "Stuff", "Pizza"];
   return (
-    <h1 className="w-full mt-14 mb-28 text-9xl ">
-      <span
-        data-content="Develop."
-        className="
-          relative block
-          leading-none tracking-tighter
-          font-extrabold text-transparent text-center bg-clip-text 
-          bg-gradient-to-br from-gradient-1-start to-gradient-1-end 
-          before:content-[attr(data-content)] before:w-full before:z-0 before:block 
-          before:absolute before:top-0 before:bottom-0 before:left-0 
-          before:text-center before:text-white
-          before:animate-faded-background-1
-        "
-      >
-        <span className="block animate-faded-foreground-1 relative z-10">Develop.</span>
-      </span>
-      <span
-        data-content="Preview."
-        className="
-          relative block
-          leading-none tracking-tighter
-          font-extrabold text-transparent text-center bg-clip-text 
-          bg-gradient-to-br from-gradient-2-start to-gradient-2-end 
-          before:content-[attr(data-content)] before:w-full before:z-0 before:block 
-          before:absolute before:top-0 before:bottom-0 before:left-0 
-          before:text-center before:text-white
-          before:animate-faded-background-2
-        "
-      >
-        <span className="block animate-faded-foreground-2 relative z-10">Preview.</span>
-      </span>
-      <span
-        data-content="Ship."
-        className="
-          relative block
-          leading-none tracking-tighter
-          font-extrabold text-transparent text-center bg-clip-text 
-          bg-gradient-to-br from-gradient-3-start to-gradient-3-end 
-          before:content-[attr(data-content)] before:w-full before:z-0 before:block 
-          before:absolute before:top-0 before:bottom-0 before:left-0 
-          before:text-center before:text-white
-          before:animate-faded-background-3
-      "
-      >
-        <span className="block animate-faded-foreground-3 relative z-10">Ship.</span>
-      </span>
+    <h1 className="w-full mt-14 mb-28 text-9xl leading-none tracking-tighter font-extrabold text-center">
+      {data.map((d, i) => {
+        const order = i + 1;
+        return (
+          <span
+            key={d}
+            data-content={d}
+            className={`relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:bottom-0 before:left-0 before:text-center before:text-white before:animate-faded-background-${order}`}
+          >
+            <span
+              className={`relative z-10 px-2 text-transparent bg-clip-text bg-gradient-to-br from-gradient-${order}-start to-gradient-${order}-end animate-faded-foreground-${order}`}
+            >
+              {d}
+            </span>
+          </span>
+        );
+      })}
     </h1>
   );
 }
