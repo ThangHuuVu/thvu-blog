702fb7a6d4ff4fcb0949a369373a023389c6c06e
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-26 15:29:32 +0700

    Rework some styles for endorsements

--- components/skills/SkillBadge.tsx
+++ components/skills/SkillBadge.tsx
@@ -4,7 +4,6 @@ import ErrorMessage from "../ErrorMessage";
 import SuccessMessage from "../SuccessMessage";
 import { Skill } from "@/lib/types/skill";
 import LoadingSpinner from "../LoadingSpinner";
-import ArrowButton from "./arrow-up-circle.svg";
 
 enum STATE {
   INITIAL,
@@ -46,25 +45,25 @@ export default function SkillCard({ skill, user }: Props) {
   }
 
   return (
-    <div className="mb-4">
-      <button
-        className="flex items-center text-lg rounded-lg px-2 py-1 font-semibold text-white bg-primary-400 dark:bg-primary-600 hover:bg-gray-700 disabled:bg-gray-700 dark:hover:bg-gray-600 dark:disabled:bg-gray-600 disabled:hover:cursor-not-allowed"
-        onClick={() => onEndorse(skill.id)}
-        disabled={!Boolean(user) || state === STATE.SUCCESS}
-      >
-        {state === STATE.LOADING ? (
-          <LoadingSpinner />
-        ) : (
-          <ArrowButton className="inline stroke-current mr-1" />
-        )}
-        {skill.name}
-      </button>
+    <div className="flex flex-col">
+      {state === STATE.LOADING ? (
+        <LoadingSpinner />
+      ) : (
+        <button
+          className="truncate flex items-center w-full rounded-lg px-4 py-2 text-lg font-semibold text-white bg-primary-400 dark:bg-primary-600 hover:bg-gray-700 disabled:bg-gray-700 dark:hover:bg-gray-600 dark:disabled:bg-gray-600 disabled:hover:cursor-not-allowed"
+          onClick={() => onEndorse(skill.id)}
+          disabled={!Boolean(user) || state === STATE.SUCCESS}
+        >
+          {skill.name}
+        </button>
+      )}
+
       {skill.people.length > 0 && (
-        <span>
-          <strong>{skill.people.length}</strong>{" "}
-          {`Endorsement${skill.people.length > 1 ? "s" : ""}`} from{" "}
-          <span>{skill.people.join(", ")}</span>
-        </span>
+        <p className="text-base text-gray-600 dark:text-gray-400">
+          <strong className="text-black dark:text-white">{skill.people.length}</strong>{" "}
+          {`${skill.name} endorsement${skill.people.length > 1 ? "s" : ""}`} from{" "}
+          <span className="text-black dark:text-white">{skill.people.join(", ")}</span>
+        </p>
       )}
       {state === STATE.ERROR && <ErrorMessage>An unexpected error occurred.</ErrorMessage>}
       {state === STATE.SUCCESS && (
