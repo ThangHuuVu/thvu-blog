1fc42b36d89a3539ae5b3ad03f86f71833d9766b
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-30 12:11:11 +0700

    Menu with transition

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,7 @@ import siteMetadata from "@/data/siteMetadata";
 import Link from "./Link";
 import ThemeSwitch from "./ThemeSwitch";
 import { useEffect, useState } from "react";
+import MenuButton from "./MenuButton";
 
 function useIsScrollTop() {
   const [isTop, setIsTop] = useState(true);
@@ -61,39 +62,14 @@ export default function NavBar() {
             </div>
             <ThemeSwitch />
             <div className="sm:hidden flex items-center">
-              <button
-                type="button"
-                className="w-8 h-8 ml-1 mr-1 rounded"
-                aria-label="Toggle Menu"
-                onClick={onToggleNav}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="text-gray-900 dark:text-gray-100"
-                >
-                  {navShow ? (
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  ) : (
-                    <path
-                      fillRule="evenodd"
-                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                      clipRule="evenodd"
-                    />
-                  )}
-                </svg>
-              </button>
+              <MenuButton onClick={onToggleNav} isOpened={navShow} />
             </div>
           </div>
         </nav>
       </header>
       {/* Mobile side menu */}
       <div
+        onClick={onToggleNav}
         className={`md:hidden fixed w-full h-screen right-0 bg-white dark:bg-black z-20 transform ease-in-out duration-300 ${
           navShow ? "translate-x-0" : "translate-x-full"
         } backdrop-filter bg-opacity-30 dark:bg-opacity-30 backdrop-saturate-150 backdrop-blur-lg firefox:bg-opacity-100 dark:firefox:bg-opacity-100`}
