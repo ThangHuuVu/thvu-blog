45c39f05750b1d4faafad47452f404ad03ec8b46
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-30 12:37:46 +0700

    Update MenuButton.tsx

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -11,7 +11,7 @@ export default function MenuButton({ isOpened, onClick }: Props) {
       onClick={onClick}
       aria-label="Toggle Menu"
     >
-      <svg viewBox="0 0 100 100" className="text-black dark:text-white">
+      <svg viewBox="0 0 100 100" className="text-gray-900 dark:text-gray-100">
         <path
           className={`${isOpened ? "opened" : ""} line line1`}
           d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
