86130a2bc4144583cbf639e6018b93866b3b77f6
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-10-04 17:40:57 +0700

    Feat: Prisma adapter for Next Auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,8 +2,11 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import LineProvider from "next-auth/providers/line";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
