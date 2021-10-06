1f5a3fd1f473dab6e19776d46a500539d98a5c6c
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-10-06 13:32:53 +0700

    Fix spacing in about

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,7 +20,7 @@ export default function About({ about }: InferGetStaticPropsType<typeof getStati
         <PageTitle>About</PageTitle>
       </div>
       <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-        <div className="flex flex-col items-center pt-8 space-x-2 xl:sticky xl:top-8">
+        <div className="flex flex-col items-center pt-8 xl:sticky xl:top-8">
           <Image
             src={profilepicture.url}
             width={192}
