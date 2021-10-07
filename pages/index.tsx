2b7bad405efaa72f8d8f4fa4faf99f4d99d493d1
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-10-07 11:01:21 +0700

    Update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,11 +41,24 @@ export default function Home({ recentPosts }: InferGetStaticPropsType<typeof get
               <Link
                 key={slug}
                 href={`/blog/${slug}`}
-                className=" w-full flex flex-col justify-between row-span-1 p-4 border-4 border-solid rounded-lg border-black dark:border-white transform hover:scale-[1.02] hover:border-primary-600 dark:hover:border-primary-400"
+                className=" w-full flex flex-col justify-between row-span-1 p-4 border-4 border-solid rounded-lg border-gray-800 dark:border-gray-200 transform hover:scale-[1.02] hover:border-primary-600 dark:hover:border-primary-400"
               >
                 <h4 className="text-lg font-bold leading-4 tracking-tight">{title}</h4>
-                <div className="pt-4 text-sm prose text-gray-500 max-w-none dark:text-gray-400">
-                  {viewCount} views
+                <div className="pt-4 text-sm prose text-gray-800 max-w-none dark:text-gray-200 flex items-center gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="h-6 w-6 inline-block fill-current"
+                  >
+                    <g data-name="Layer 2">
+                      <g data-name="eye">
+                        <rect width="24" height="24" opacity="0" />
+                        <path d="M21.87 11.5c-.64-1.11-4.16-6.68-10.14-6.5-5.53.14-8.73 5-9.6 6.5a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25c5.53-.14 8.74-5 9.6-6.5a1 1 0 0 0 0-1zM12.22 17c-4.31.1-7.12-3.59-8-5 1-1.61 3.61-4.9 7.61-5 4.29-.11 7.11 3.59 8 5-1.03 1.61-3.61 4.9-7.61 5z" />
+                        <path d="M12 8.5a3.5 3.5 0 1 0 3.5 3.5A3.5 3.5 0 0 0 12 8.5zm0 5a1.5 1.5 0 1 1 1.5-1.5 1.5 1.5 0 0 1-1.5 1.5z" />
+                      </g>
+                    </g>
+                  </svg>
+                  {viewCount}
                 </div>
               </Link>
             );
