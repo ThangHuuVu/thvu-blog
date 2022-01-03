ce014425d0757a1b48238c7c58708fb027022e68
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2022-01-03 10:43:30 +0700

    feat: add ISR for endorsement page

--- pages/endorsements.tsx
+++ pages/endorsements.tsx
@@ -12,6 +12,7 @@ export async function getStaticProps() {
     props: {
       fallbackData: skillsByCategory,
     },
+    revalidate: 60,
   };
 }
 export default function EndorsementsPage({
@@ -24,7 +25,7 @@ export default function EndorsementsPage({
         description={"THVu's Endorsements"}
       />
 
-      <div className="flex flex-col justify-center items-start max-w-2xl pt-6 pb-8 space-y-2 md:space-y-5">
+      <div className="flex flex-col items-start justify-center max-w-2xl pt-6 pb-8 space-y-2 md:space-y-5">
         <PageTitle>Endorsements</PageTitle>
         <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 xl:text-xl">
           Since you're here, I invite you to give me endorsement(s) based on the experience you had
