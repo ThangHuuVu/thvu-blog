6d18272c005ca07fe268714f7d4d5a456631aec4
Thang Vu <thvu@hey.com>
2022-02-24 15:22:50 +0700

    Move GA to _app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "@/lib/gtag";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
           {/* font */}
           <link
             href="/fonts/be-vietnam-pro-v2-vietnamese_latin-regular.woff2"
