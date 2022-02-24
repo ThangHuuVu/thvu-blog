8e46427483bcfe8d9e37868507a49bf76924fe6c
Thang Vu <thvu@hey.com>
2022-02-24 15:23:05 +0700

    Update CSP

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   siteUrl: "https://thvu.dev",
   siteRepo: "https://github.com/ThangHuuVu/thvu-blog",
   siteLogo: "/static/images/logo.png",
-  image: "https://pbs.twimg.com/profile_images/1440275614338932737/6BPGljnG_400x400.jpg",
+  image: "https://pbs.twimg.com/profile_images/1493051899158794242/91wCup31_400x400.jpg",
   socialBanner: "/static/images/social-banner.png",
   email: "thvu@hey.com",
   github: "https://github.com/ThangHuuVu",
--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,22 +3,22 @@ import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.twitter.com https://www.googletagmanager.com https://www.google-analytics.com https://dbdiagram.io;
-  child-src *.youtube.com *.google.com *.twitter.com  https://codepen.io https://dbdiagram.io;
+  default-src 'self' data:;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.twitter.com https://www.googletagmanager.com https://www.google-analytics.com;
+  child-src *.youtube.com *.google.com *.twitter.com https://cdpn.io https://dbdiagram.io;
   style-src 'self' 'unsafe-inline' *.googleapis.com;
-  img-src * blob: data: www.googletagmanager.com;
-  media-src 'self';
+  img-src * blob: data: https://www.googletagmanager.com;
   connect-src *;
-  font-src 'self';
   object-src 'none';
-  worker-src 'self' *.youtube.com *.google.com *.twitter.com;
+  form-action 'self';
+  frame-ancestors 'none';
+  base-uri 'none';
   `;
 
   const response = NextResponse.next();
 
   response.headers.set("Content-Security-Policy", ContentSecurityPolicy.replace(/\n/g, ""));
-  response.headers.set("Referrer-Policy", "origin-when-cross-origin");
+  response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
   response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
   response.headers.set("X-Frame-Options", "DENY");
   response.headers.set("X-Content-Type-Options", "nosniff");
