0b9fb477e3a1cee6e910e7c67b22ec897c11c592
Thang Vu <thvu@hey.com>
2022-02-24 21:30:08 +0700

    Attempt to use nonce

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,28 +2,14 @@ import type { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const ContentSecurityPolicy = `
-  default-src 'self' data:;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.twitter.com https://www.googletagmanager.com https://www.google-analytics.com;
-  child-src *.youtube.com *.google.com *.twitter.com https://cdpn.io https://codepen.io https://dbdiagram.io;
-  style-src 'self' 'unsafe-inline' *.googleapis.com;
-  img-src * blob: data: https://www.googletagmanager.com;
-  worker-src 'self' *.youtube.com *.google.com *.twitter.com;
-  connect-src *;
-  object-src 'none';
-  form-action 'self';
-  frame-ancestors 'none';
-  base-uri 'none';
-  `;
-
   const response = NextResponse.next();
 
-  response.headers.set("Content-Security-Policy", ContentSecurityPolicy.replace(/\n/g, ""));
   response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
   response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
   response.headers.set("X-Frame-Options", "DENY");
   response.headers.set("X-Content-Type-Options", "nosniff");
   response.headers.set("X-DNS-Prefetch-Control", "on");
+  response.headers.set("Permissions-Policy", "geolocation=(self), microphone=()");
 
   return response;
 }
