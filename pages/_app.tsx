6bda14880fdf99812ebe68c171d2b7c12fbdba6c
Thang Vu <thvu@hey.com>
2022-02-24 15:22:20 +0700

    Move GA to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,8 @@ import { SessionProvider } from "next-auth/react";
 import LayoutWrapper from "@/components/LayoutWrapper";
 import { pageView } from "@/lib/gtag";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "@/lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -24,18 +26,39 @@ export default function App({ Component, pageProps }: AppProps) {
   const { session, ...rest } = pageProps;
 
   return (
-    <ThemeProvider attribute="class">
-      <SessionProvider session={session}>
-        <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-          />
-        </Head>
-        <LayoutWrapper>
-          <Component {...rest} />
-        </LayoutWrapper>
-      </SessionProvider>
-    </ThemeProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ThemeProvider attribute="class">
+        <SessionProvider session={session}>
+          <Head>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+            />
+          </Head>
+          <LayoutWrapper>
+            <Component {...rest} />
+          </LayoutWrapper>
+        </SessionProvider>
+      </ThemeProvider>
+    </>
   );
 }
