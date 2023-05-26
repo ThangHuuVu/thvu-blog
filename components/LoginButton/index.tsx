2c00ea74009f795e1e807d39fc9150d4888a2886
Thang Vu <hi@thvu.dev>
2023-05-26 07:59:36 +0700

    feat: add email provider (#163)

--- components/LoginButton/index.tsx
+++ components/LoginButton/index.tsx
@@ -1,4 +1,4 @@
-import { ClientSafeProvider, signIn } from "@auth/nextjs/client";
+import { signIn, getCsrfToken } from "@auth/nextjs/client";
 import Line from "./line.svg";
 import Google from "./google.svg";
 import GoogleDark from "./google-dark.svg";
@@ -6,8 +6,9 @@ import Github from "./github.svg";
 import GithubDark from "./github-dark.svg";
 import { useDarkTheme } from "@/lib/hooks/useDarkTheme";
 import { useRouter } from "next/router";
+import { useState } from "react";
 interface StyleGuide {
-  Logo: any;
+  Logo?: any;
   LogoDark?: any;
   bg: string;
   bgDark?: string;
@@ -36,20 +37,53 @@ const providerStyleGuides: { [key: string]: StyleGuide } = {
     text: "text-black",
     textDark: "text-white",
   },
+  sendgrid: {
+    bg: "bg-black",
+    bgDark: "bg-white",
+    text: "text-white",
+    textDark: "text-black",
+  },
 };
 
-export default function LoginButton({ provider }: { provider: ClientSafeProvider }) {
+export default function LoginButton({ provider }: { provider: any }) {
   const [isDark, mounted] = useDarkTheme();
-  const { Logo, LogoDark, bg, bgDark, text, textDark } = providerStyleGuides[provider.id];
+  const [csrf, setCsrf] = useState("");
   const router = useRouter();
   const { callbackUrl } = router.query;
+  getCsrfToken().then(setCsrf);
+
   if (!mounted) return null;
+  const { Logo, LogoDark, bg, bgDark, text, textDark } = providerStyleGuides[provider.id];
+
+  if (provider.type === "email")
+    return (
+      <form action={provider.signinUrl} method="POST" key={provider.name}>
+        <input
+          className="w-full h-12 mb-4 pl-2 rounded-lg text-slate-700 dark:text-slate-300 dark:bg-slate-800 border border-slate-300 dark:border-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400"
+          placeholder="Your email"
+          type="email"
+          required
+          id="email"
+          name="email"
+        ></input>
+        <input type="hidden" name="csrfToken" value={csrf} />
+        <button
+          className={`w-56 h-12 flex items-center justify-center gap-3 text-sm font-medium leading-5 transition-colors duration-150 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-primary
+          ${isDark && bgDark ? bgDark : bg}
+          ${isDark && textDark ? textDark : text} `}
+          type="submit"
+        >
+          Sign in with Email
+        </button>
+      </form>
+    );
+
   return (
     <div key={provider.name}>
       <button
         className={`w-56 h-12 flex items-center gap-3 text-sm font-medium leading-5 transition-colors duration-150 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-primary
-         ${isDark && bgDark ? bgDark : bg}
-         ${isDark && textDark ? textDark : text} `}
+          ${isDark && bgDark ? bgDark : bg}
+          ${isDark && textDark ? textDark : text} `}
         onClick={() => signIn(provider.id, { callbackUrl: callbackUrl as string })}
       >
         {isDark && LogoDark ? <LogoDark className="h-full p-2" /> : <Logo className="h-full p-2" />}
