cf72e18807da634c96f97dd73c98a0271cfd91a3
Thang Vu <hi@thvu.dev>
2023-05-28 13:32:16 +0700

    remove email temporarily

--- auth.ts
+++ auth.ts
@@ -15,41 +15,41 @@ export const { handlers, auth } = NextAuth({
     GitHub,
     // @ts-expect-error
     Line,
-    // @ts-expect-error
-    {
-      id: "sendgrid",
-      type: "email",
-      async sendVerificationRequest({ identifier: email, url }) {
-        // Call the cloud Email provider API for sending emails
-        // See https://docs.sendgrid.com/api-reference/mail-send/mail-send
-        const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
-          // The body format will vary depending on provider, please see their documentation
-          // for further details.
-          body: JSON.stringify({
-            personalizations: [{ to: [{ email }] }],
-            from: { email: "noreply@thvu.dev" },
-            subject: "Sign in to thvu.dev",
-            content: [
-              {
-                type: "text/plain",
-                value: `Please click here to authenticate - ${url}`,
-              },
-            ],
-          }),
-          headers: {
-            // Authentication will also vary from provider to provider, please see their docs.
-            Authorization: `Bearer ${process.env.SENDGRID_API}`,
-            "Content-Type": "application/json",
-          },
-          method: "POST",
-        });
+    // TODO: Enable this again when SendGrid allows me to send emails
+    // {
+    //   id: "sendgrid",
+    //   type: "email",
+    //   async sendVerificationRequest({ identifier: email, url }) {
+    //     // Call the cloud Email provider API for sending emails
+    //     // See https://docs.sendgrid.com/api-reference/mail-send/mail-send
+    //     const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
+    //       // The body format will vary depending on provider, please see their documentation
+    //       // for further details.
+    //       body: JSON.stringify({
+    //         personalizations: [{ to: [{ email }] }],
+    //         from: { email: "noreply@thvu.dev" },
+    //         subject: "Sign in to thvu.dev",
+    //         content: [
+    //           {
+    //             type: "text/plain",
+    //             value: `Please click here to authenticate - ${url}`,
+    //           },
+    //         ],
+    //       }),
+    //       headers: {
+    //         // Authentication will also vary from provider to provider, please see their docs.
+    //         Authorization: `Bearer ${process.env.SENDGRID_API}`,
+    //         "Content-Type": "application/json",
+    //       },
+    //       method: "POST",
+    //     });
 
-        if (!response.ok) {
-          const { errors } = await response.json();
-          throw new Error(JSON.stringify(errors));
-        }
-      },
-    },
+    //     if (!response.ok) {
+    //       const { errors } = await response.json();
+    //       throw new Error(JSON.stringify(errors));
+    //     }
+    //   },
+    // },
   ],
   pages: {
     signIn: "/auth/signin",
